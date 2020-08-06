7af4b057fa4edc2c6e6e99a9a019a8ecb382dfc8
Dakota Coppage <dakotakcoppage@me.com>
2020-08-06 04:59:14 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,7 @@ def badge_maker(speaker)
   return "Hello, my name is #{speaker}."
 end
 
-list_of_speakers = [""]
+list_of_speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def batch_badge_creater(list_of_speakers)
   return 
